9e194bbd9618a1393662607407ce8cc1660db8cd
AntNowak <ant.j.nowak@gmail.com>
2026-05-23 13:55:45 +0800

    cooldown detection

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player (CircleShape):
     def __init__(self, x: float, y :float, PLAYER_RADIUS):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0.0
+        self.cooldown_timer = 0
     
     def draw (self, screen: pygame.Surface):
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH )
@@ -25,6 +26,7 @@ class Player (CircleShape):
     
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
+        self.cooldown_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -36,7 +38,9 @@ class Player (CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
-    
+
+                
+                
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
@@ -44,8 +48,13 @@ class Player (CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        unit_vector = pygame.Vector2(0,1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
-        shot.velocity = rotated_with_speed_vector
+        if self.cooldown_timer > 0:
+            return
+        else:
+            shot = Shot(self.position.x, self.position.y)
+            unit_vector = pygame.Vector2(0,1)
+            rotated_vector = unit_vector.rotate(self.rotation)
+            rotated_with_speed_vector = rotated_vector * PLAYER_SHOOT_SPEED
+            shot.velocity = rotated_with_speed_vector
+            self.cooldown_timer = PLAYER_SHOOT_COOLDOWN_SECONDS
+    
