e52970ce08420bc125edae2b7c244275b88b9efd
AntNowak <ant.j.nowak@gmail.com>
2026-05-23 13:19:34 +0800

    added shot group

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from player import *
+from shot import *
 from asteroid import *
 from asteroidfield import *
 from constants import *
@@ -14,9 +15,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     astroid_field = AsteroidField()
