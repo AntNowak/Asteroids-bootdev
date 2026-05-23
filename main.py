dc4a2f77bb357d22512a1556ba462456b29c2c3d
AntNowak <ant.j.nowak@gmail.com>
2026-05-23 13:55:12 +0800

    added kill

--- main.py
+++ main.py
@@ -39,11 +39,18 @@ def main():
 
         updatable.update(dt)
 
+
         for asteroid in asteroids:
             if asteroid.collides_with(player):
                 log_event("player_hit")
                 print ("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
 
         for obj in drawable:
             obj.draw(screen)
